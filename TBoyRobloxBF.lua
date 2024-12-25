321c9cf4c760601d8e75481b8a21bfdb4bdfd0b0
TBoyRoblox <trieutuyen802@gmail.com>
2024-12-25 16:26:56 +0700

    Add files via upload

---
+++ TBoyRobloxBF.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "TBoyRoblox",--Tên Script
+    logoscript = "102729459546242 ",--ID Logo Script
+    tacgia = "TBoyRobloxYT"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/516a5669fc39b4945cd0609a08264505.lua"))()
